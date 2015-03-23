4e29da5fcfe09c7d427536feda6d2e0bbdc51c27
Alexander Bliskovsky <alexander.bliskovsky@gmail.com>
2015-03-22 22:49:56 -0400

    Add requirement that database path must be absolute.

--- src/run_server.py
+++ src/run_server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 from os import path
 from tictail_todo import app, db
 
@@ -13,6 +14,11 @@ def main():
                         help=("Path to the sqlite db file to use. The file will be created" +
                               " if it does not exist."))
     args = parser.parse_args()
+
+    if not path.isabs(args.db_path):
+        print("Error: Database path must be absolute.", file=sys.stderr)
+        sys.exit(1)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + args.db_path
     ensure_db_exists(args.db_path)
     app.run(host="0.0.0.0")
